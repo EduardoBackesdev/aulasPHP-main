118c1ce68bff0bdcccbf2a19f86af35b47299e95
Eduardobackesdev <eduardo.eduardo.programacao@hotmail.com>
2023-12-25 03:07:08 -0300

    crud

---
+++ Bd/crud.php
@@ -0,0 +1,56 @@
+<?php
+
+
+// inserir
+
+$pdo = new PDO('mysql:host=localhost;dbname=aulasphp;', 'root', '');
+$sql = $pdo->prepare("INSERT INTO `pessoas` VALUES (null, 'Eduardo', 'Backes', 172)");
+// $sql->execute();
+
+// inserir 
+
+
+// atualizar
+
+$pdo = new PDO('mysql:host=localhost;dbname=aulasphp', 'root', ''); // de novo para treinar
+$sql = $pdo->prepare("UPDATE `pessoas` SET nome='Andriele', sobrenome='marcolin', altura='165' WHERE id=4");
+// $sql->execute();
+
+// atualizar
+
+
+// deletar
+
+$pdo = new PDO("mysql:host:localhost;dbname=aulasphp", 'root', '');
+$sql = $pdo->prepare("DELETE FROM `pessoas`  ");
+// $sql->execute();
+
+// deletar
+
+
+// selecionar
+
+$pdo = new PDO("mysql:host=localhost;dbname=aulasphp", 'root','');
+$sql = $pdo->prepare();
+$sql->execute();
+
+// selecionar
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
